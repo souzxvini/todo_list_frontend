79bd96d0b8db95652c9fe487c3b73c6e31fc7edb
Vinicius Souza <vnsoliveira2512@gmail.com>
2026-01-09 16:50:49 -0300

    ajuste env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   appUrl: 'https://d1z9kx11cv7jnv.cloudfront.net',
+  apiUrl: 'http://localhost:3000',
   cognitoLogoutUrl: 'https://us-east-174rxy2but.auth.us-east-1.amazoncognito.com/logout'
 };
 
