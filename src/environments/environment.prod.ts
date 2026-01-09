d2ca8db770c588406fef3747618f2453c9714a17
Vinicius Souza <vnsoliveira2512@gmail.com>
2026-01-08 23:55:12 -0300

    cerw]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  // Este valor será usado apenas como fallback
-  // Em runtime, usamos window.location.origin via getAppUrl()
-  appUrl: 'd1z9kx11cv7jnv.cloudfront.net', // Será substituído automaticamente em runtime
+  appUrl: 'https://d1z9kx11cv7jnv.cloudfront.net',
   cognitoLogoutUrl: 'https://us-east-174rxy2but.auth.us-east-1.amazoncognito.com/logout'
 };
 
