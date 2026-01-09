bba828b7362433ef3fc3fee0d780b3850866aec5
Vinicius Souza <vnsoliveira2512@gmail.com>
2026-01-09 17:03:02 -0300

    ajuste guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { map, take } from 'rxjs';
+import { map, switchMap, take } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const oidcSecurityService = inject(OidcSecurityService);
   const router = inject(Router);
   
-  return oidcSecurityService.isAuthenticated$.pipe(
+  // Verifica se a autenticação foi completada antes de validar
+  return oidcSecurityService.checkAuth().pipe(
     take(1),
     map(({ isAuthenticated }) => {
       if (isAuthenticated) {
